8a52939742ac7376de2743331a8fb28b238199f4
dondigidol <pillar_box@bk.ru>
2018-03-21 19:33:55 +0300

    убрал текст запроса, в последующем планирую отказаться от этого класса контрорллера, так как для будующих изменений лучше динамическое изменение форм

--- src/ru/whoisthere/view/personsOverviewController.java
+++ src/ru/whoisthere/view/personsOverviewController.java
@@ -1,63 +1,80 @@
 package ru.whoisthere.view;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.Border;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import ru.whoisthere.SqlUtils;
-import ru.whoisthere.model.PersonsData;
+import ru.whoisthere.model.Person;
 
 public class personsOverviewController {
 	
-	static ArrayList<ArrayList<String>> allPersonsData = null;
+	private List<Person> persons = new ArrayList<Person>();
 	public GridPane gp = new GridPane();
+
+	
+	
 	
 	@FXML
 	public Button btn;
+	
+	@FXML
+	public VBox firstColumn = new VBox();
+	public VBox secondColumn = new VBox();
+	public VBox thirdColumn = new VBox();
+	public VBox fourthColumn = new VBox();
+	public VBox fifthColumn = new VBox();
+	public VBox sixthColumn = new VBox();
+	public VBox seventhColumn = new VBox();
+	public VBox eighthColumn = new VBox();
+	public VBox ninthColumn = new VBox();
+	public VBox tenthColumn = new VBox();
+	public VBox eleventhColumn = new VBox();
+	public VBox twelfthColumn = new VBox();
+	public VBox thirteenthColumn = new VBox();
+	public VBox fourteenthColumn = new VBox();
+	public VBox fifteenthColumn = new VBox();
+	public VBox sixteenthColumn = new VBox();
+	
+	
 
-	public void uploadData() {
+	public void uploadData(){		
+		//gp.getColumnConstraints().clear();
 		
-		String queryStr = 	"SELECT plist.FirstName, plist.Name, p.TimeVal, p.HozOrgan, plist.Section, div.Name, plist.Picture " +
-				"FROM (SELECT MAX(TimeVal) as TimeVal, HozOrgan, Max(Mode) as mode FROM ORION1122.dbo.pLogData Where TimeVal>='20180213' Group By HozOrgan) as p " +
-				"INNER JOIN (SELECT ID, Name, FirstName, Section, Picture From ORION1122.dbo.pList  where Section <> '199' and Company=2) plist ON plist.ID = p.HozOrgan " +
-				"LEFT JOIN [ORION1122].[dbo].[PDivision] div ON div.ID = plist.Section " +
-				"WHERE TimeVal >='20180213' and p.Mode = 1 and div.ID < 184 " +
-				"Order by div.Name";
-
 		SqlUtils sqlutil = new SqlUtils();
 		if (sqlutil.openConnection("10.84.79.125", "sa", "123456")) {
-			allPersonsData = sqlutil.execQuery(queryStr);
+			persons = sqlutil.getResultArray();
 		}
 		
-		PersonsData pd = new PersonsData(allPersonsData);
-		gp.setColumnIndex(pd.getSecondColumn(), 0);
-		/*gp.getChildren().addAll(1, pd.getSecondColumn());
-		gp.getChildren().addAll(2, pd.getThirdColumn());
-		gp.getChildren().addAll(3, pd.getFourthColumn());
-		gp.getChildren().addAll(4, pd.getFifthColumn());
-		gp.getChildren().addAll(5, pd.getSixthColumn());
-		gp.getChildren().addAll(6, pd.getSeventhColumn());
-		gp.getChildren().addAll(7, pd.getEighthColumn());
-		gp.getChildren().addAll(8, pd.getNinthColumn());
-		gp.getChildren().addAll(9, pd.getTenthColumn());
-		gp.getChildren().addAll(10, pd.getEleventhColumn());
-		gp.getChildren().addAll(11, pd.getTwelthColumn());
-		gp.getChildren().addAll(12, pd.getThirtinthColumn());
-		gp.getChildren().addAll(13, pd.getFourtinthColumn());
-		gp.getChildren().addAll(14, pd.getFiftinthColumn());
-		gp.getChildren().addAll(15, pd.getSixtinthColumn());*/
 		
+		for (Person person : persons) {
+			VBox personInfoContainer = new VBox();
+			ImageView iv = new ImageView();
+			Image image = SwingFXUtils.toFXImage(person.getPhoto(), null);
+			
+			iv.setFitWidth(100);
+			iv.setFitHeight(150);
+			iv.setImage(image);
+			
+			personInfoContainer.getChildren().add(iv);
+			personInfoContainer.getChildren().add(new Label (person.getName()));
+			personInfoContainer.getChildren().add(new Label (person.getSurname()));
+			personInfoContainer.setAlignment(Pos.CENTER);
 		
+		gp.getChildren().add(0, firstColumn);
+		}	
 	}
-	
-	
-	
-	
-	
-	
 }
+
