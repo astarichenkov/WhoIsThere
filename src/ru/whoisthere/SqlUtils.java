d1b1cf4a616a84c5867af4b2d5c9b974ebf023a2
dondigidol <pillar_box@bk.ru>
2018-03-30 09:27:14 +0300

    добавил автоматическую подстановку текущего дня в запрос

--- src/ru/whoisthere/SqlUtils.java
+++ src/ru/whoisthere/SqlUtils.java
@@ -3,7 +3,9 @@ package ru.whoisthere;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import ru.whoisthere.model.Person;
@@ -40,13 +42,15 @@ public class SqlUtils {
 	}
 	
 	public List<Person> execQuery(){
-		
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
+		String curDate = dateFormat.format(new Date()); 
+	
 		List<Person> persons = new ArrayList<Person>();
 		String queryStr = 	"SELECT plist.FirstName, plist.Name, div.Name, plist.Picture " +
-				"FROM (SELECT MAX(TimeVal) as TimeVal, HozOrgan, Max(Mode) as mode FROM ORION1122.dbo.pLogData Where TimeVal>='20180326' Group By HozOrgan) as p " +
+				"FROM (SELECT MAX(TimeVal) as TimeVal, HozOrgan, Max(Mode) as mode FROM ORION1122.dbo.pLogData Where TimeVal>='" + curDate + "' Group By HozOrgan) as p " +
 				"INNER JOIN (SELECT ID, Name, FirstName, Section, Picture From ORION1122.dbo.pList  where Section <> '199' and Company=2) plist ON plist.ID = p.HozOrgan " +
 				"LEFT JOIN [ORION1122].[dbo].[PDivision] div ON div.ID = plist.Section " +
-				"WHERE TimeVal >='20180326' and p.Mode = 1 and div.ID < 184 " +
+				"WHERE TimeVal >='" + curDate + "' and p.Mode = 1 and div.ID < 184 " +
 				"Order by plist.Name";
 		try {
 			Statement stmt = con.createStatement();			
