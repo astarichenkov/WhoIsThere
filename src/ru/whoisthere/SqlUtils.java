e69543b55e2bcc26a2dea78f6b102d53cb9c3d81
dondigidol <pillar_box@bk.ru>
2018-04-27 09:48:33 +0300

    удалил ненужный код, изменил сортировку в запросе

--- src/ru/whoisthere/SqlUtils.java
+++ src/ru/whoisthere/SqlUtils.java
@@ -46,18 +46,12 @@ public class SqlUtils {
 		String curDate = dateFormat.format(new Date()); 
 	
 		List<Person> persons = new ArrayList<Person>();
-/*		String queryStr = 	"SELECT plist.FirstName, plist.Name, div.Name, plist.Picture " +
-				"FROM (SELECT MAX(TimeVal) as TimeVal, HozOrgan, Max(Mode) as mode FROM ORION1122.dbo.pLogData Where TimeVal>='" + curDate + "' Group By HozOrgan) as p " +
-				"INNER JOIN (SELECT ID, Name, FirstName, Section, Picture From ORION1122.dbo.pList  where Section <> '199' and Company=2) plist ON plist.ID = p.HozOrgan " +
-				"LEFT JOIN [ORION1122].[dbo].[PDivision] div ON div.ID = plist.Section " +
-				"WHERE TimeVal >='" + curDate + "' and p.Mode = 1 and div.ID < 184 " +
-				"Order by plist.Name";*/
 		String queryStr = "SELECT pList.FirstName, pList.Name, pDiv.Name, pList.Picture " +
 				"FROM (SELECT MAX(TimeVal) as TimeVal, HozOrgan FROM ORION1122.dbo.pLogData Where TimeVal>='" + curDate + "' and Event='32' and DoorIndex='15' Group by HozOrgan) as pLog " +
 				"INNER JOIN (SELECT TimeVal, HozOrgan, Mode From ORION1122.dbo.pLogData Where Event='32' and DoorIndex='15' and TimeVal>='" + curDate + "' and Mode=1) maxMode ON maxMode.HozOrgan = pLog.HozOrgan and maxMode.TimeVal = pLog.TimeVal " +
 				"INNER JOIN (SELECT ID, Name, FirstName, Section, Picture From ORION1122.dbo.pList where TabNumber <> '' and Company=2) pList ON pList.ID = pLog.HozOrgan " +
 				"INNER JOIN (SELECT ID, Name FROM [ORION1122].[dbo].[PDivision] Where GroupID='1') pDiv ON pDiv.ID = pList.Section " +
-				"Order By maxMode.Mode";
+				"Order By pList.Name";
 		try {
 			Statement stmt = con.createStatement();			
 			ResultSet rs = stmt.executeQuery(queryStr);
