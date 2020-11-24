ec410105a1ad551bb52f056f08aa9e57e513aac8
Anton Starichenkov <anton.starichenkov@gmail.com>
2020-11-24 09:24:02 +0300

    DepartsOverviewController добавил properties

--- WhoIsThere-intellij_project/src/ru/whoisthere/controller/DepartsOverviewController.java
+++ WhoIsThere-intellij_project/src/ru/whoisthere/controller/DepartsOverviewController.java
@@ -6,12 +6,9 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import ru.whoisthere.model.Departments;
-import ru.whoisthere.settings.ConnectionSettings;
 
 import java.io.*;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
-import java.util.Base64;
 import java.util.Properties;
 import java.util.ResourceBundle;
 
@@ -112,73 +109,73 @@ public class DepartsOverviewController implements Initializable {
     }
 
     public void saveAndExit() {
-            File file = new File("departs.properties");
-            try {
-                file.setExecutable(false);
-                file.setReadable(true);
-                file.setWritable(true);
-            } catch (SecurityException e) {
-                addInfoLog(e.getMessage() + "Exception in set file attributes");
-            }
-            Properties properties = new Properties();
-            properties.put("departKey00", escapeHtml4(departLabel00.getText()));
-            properties.put("departKey01", escapeHtml4(departLabel01.getText()));
-            properties.put("departKey02", escapeHtml4(departLabel02.getText()));
-            properties.put("departKey03", escapeHtml4(departLabel03.getText()));
-            properties.put("departKey04", escapeHtml4(departLabel04.getText()));
-            properties.put("departKey05", escapeHtml4(departLabel05.getText()));
-            properties.put("departKey06", escapeHtml4(departLabel06.getText()));
-            properties.put("departKey07", escapeHtml4(departLabel07.getText()));
-            properties.put("departKey08", escapeHtml4(departLabel08.getText()));
-            properties.put("departKey09", escapeHtml4(departLabel09.getText()));
-            properties.put("departKey10", escapeHtml4(departLabel10.getText()));
-            properties.put("departKey11", escapeHtml4(departLabel11.getText()));
-            properties.put("departKey12", escapeHtml4(departLabel12.getText()));
-            properties.put("departKey13", escapeHtml4(departLabel13.getText()));
-            properties.put("departKey14", escapeHtml4(departLabel14.getText()));
-            properties.put("departKey15", escapeHtml4(departLabel15.getText()));
-            writeToFile(properties);
-
-            addInfoLog("Settings was successfully recorded to file departs.properties");
-            Stage stage = (Stage) okButton.getScene().getWindow();
-            stage.close();
+        File file = new File("departs.properties");
+        try {
+            file.setExecutable(false);
+            file.setReadable(true);
+            file.setWritable(true);
+        } catch (SecurityException e) {
+            addInfoLog(e.getMessage() + "Exception in set file attributes");
         }
+        Properties properties = new Properties();
+        properties.put("depart0", escapeHtml4(departKey00.getText()));
+        properties.put("depart1", escapeHtml4(departKey01.getText()));
+        properties.put("depart2", escapeHtml4(departKey02.getText()));
+        properties.put("depart3", escapeHtml4(departKey03.getText()));
+        properties.put("depart4", escapeHtml4(departKey04.getText()));
+        properties.put("depart5", escapeHtml4(departKey05.getText()));
+        properties.put("depart6", escapeHtml4(departKey06.getText()));
+        properties.put("depart7", escapeHtml4(departKey07.getText()));
+        properties.put("depart8", escapeHtml4(departKey08.getText()));
+        properties.put("depart9", escapeHtml4(departKey09.getText()));
+        properties.put("depart10", escapeHtml4(departKey10.getText()));
+        properties.put("depart11", escapeHtml4(departKey11.getText()));
+        properties.put("depart12", escapeHtml4(departKey12.getText()));
+        properties.put("depart13", escapeHtml4(departKey13.getText()));
+        properties.put("depart14", escapeHtml4(departKey14.getText()));
+        properties.put("depart15", escapeHtml4(departKey15.getText()));
+        writeToFile(properties);
+
+        addInfoLog("Settings was successfully recorded to file departs.properties");
+        Stage stage = (Stage) okButton.getScene().getWindow();
+        stage.close();
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         Departments departments = new Departments();
-        departLabel00.setText(departments.getDepartments().get(0).get(1));
-        departLabel01.setText(departments.getDepartments().get(1).get(1));
-        departLabel02.setText(departments.getDepartments().get(2).get(1));
-        departLabel03.setText(departments.getDepartments().get(3).get(1));
-        departLabel04.setText(departments.getDepartments().get(4).get(1));
-        departLabel05.setText(departments.getDepartments().get(5).get(1));
-        departLabel06.setText(departments.getDepartments().get(6).get(1));
-        departLabel07.setText(departments.getDepartments().get(7).get(1));
-        departLabel08.setText(departments.getDepartments().get(8).get(1));
-        departLabel09.setText(departments.getDepartments().get(9).get(1));
-        departLabel10.setText(departments.getDepartments().get(10).get(1));
-        departLabel11.setText(departments.getDepartments().get(11).get(1));
-        departLabel12.setText(departments.getDepartments().get(12).get(1));
-        departLabel13.setText(departments.getDepartments().get(13).get(1));
-        departLabel14.setText(departments.getDepartments().get(14).get(1));
-        departLabel15.setText(departments.getDepartments().get(15).get(1));
+        departLabel00.setText("ЧПОК");
+        departLabel01.setText("1 отдел");
+        departLabel02.setText("2 отдел");
+        departLabel03.setText("3 отдел");
+        departLabel04.setText("4 отдел");
+        departLabel05.setText("5 отдел");
+        departLabel06.setText("6 отдел");
+        departLabel07.setText("7 отдел");
+        departLabel08.setText("8 отдел");
+        departLabel09.setText("9 отдел");
+        departLabel10.setText("10 отдел");
+        departLabel11.setText("11 отдел");
+        departLabel12.setText("12 отдел");
+        departLabel13.setText("13 отдел");
+        departLabel14.setText("14 отдел");
+        departLabel15.setText("15 отдел");
 
-        departKey00.setText(departments.getDepartments().get(0).get(0));
-        departKey01.setText(departments.getDepartments().get(1).get(0));
-        departKey02.setText(departments.getDepartments().get(2).get(0));
-        departKey03.setText(departments.getDepartments().get(3).get(0));
-        departKey04.setText(departments.getDepartments().get(4).get(0));
-        departKey05.setText(departments.getDepartments().get(5).get(0));
-        departKey06.setText(departments.getDepartments().get(6).get(0));
-        departKey07.setText(departments.getDepartments().get(7).get(0));
-        departKey08.setText(departments.getDepartments().get(8).get(0));
-        departKey09.setText(departments.getDepartments().get(9).get(0));
-        departKey10.setText(departments.getDepartments().get(10).get(0));
-        departKey11.setText(departments.getDepartments().get(11).get(0));
-        departKey12.setText(departments.getDepartments().get(12).get(0));
-        departKey13.setText(departments.getDepartments().get(13).get(0));
-        departKey14.setText(departments.getDepartments().get(14).get(0));
-        departKey15.setText(departments.getDepartments().get(15).get(0));
+        departKey00.setText(departments.getDepartments().get(0));
+        departKey01.setText(departments.getDepartments().get(1));
+        departKey02.setText(departments.getDepartments().get(2));
+        departKey03.setText(departments.getDepartments().get(3));
+        departKey04.setText(departments.getDepartments().get(4));
+        departKey05.setText(departments.getDepartments().get(5));
+        departKey06.setText(departments.getDepartments().get(6));
+        departKey07.setText(departments.getDepartments().get(7));
+        departKey08.setText(departments.getDepartments().get(8));
+        departKey09.setText(departments.getDepartments().get(9));
+        departKey10.setText(departments.getDepartments().get(10));
+        departKey11.setText(departments.getDepartments().get(11));
+        departKey12.setText(departments.getDepartments().get(12));
+        departKey13.setText(departments.getDepartments().get(13));
+        departKey14.setText(departments.getDepartments().get(14));
+        departKey15.setText(departments.getDepartments().get(15));
     }
 }
--- WhoIsThere-intellij_project/src/ru/whoisthere/model/Departments.java
+++ WhoIsThere-intellij_project/src/ru/whoisthere/model/Departments.java
@@ -1,56 +1,44 @@
 package ru.whoisthere.model;
 
 import java.io.*;
-import java.nio.file.Files;
 import java.util.*;
 
-import static org.apache.commons.text.StringEscapeUtils.escapeHtml4;
 import static ru.whoisthere.utils.Logging.addInfoLog;
 
 public class Departments {
-    private List<List<String>> departs = new ArrayList<>();
+    private List<String> departs = new ArrayList<>();
 
     public Departments() {
-        String role = "ADMIN";
-        if (role.equals("ADMIN")) {
-            File file = new File("departs.txt");
-            try {
-                file.setExecutable(false);
-                file.setReadable(true);
-                file.setWritable(true);
-            } catch (SecurityException e) {
-                addInfoLog(e.getMessage() + "Exception in set file attributes");
-            }
-
-            try {
-                List<String> lines = Files.readAllLines(file.toPath());
-                for (String s : lines) {
-                    String escaped = escapeHtml4(s);
-                    departs.add(Arrays.asList(escaped.split(", ")));
-                }
-            } catch (IOException e) {
-                addInfoLog(e.getMessage() + " File reading error departs.txt");
-            }
+        File file = new File("departs.properties");
+        Properties props = new Properties();
+        try {
+            file.setExecutable(false);
+            file.setReadable(true);
+            file.setWritable(false);
+        } catch (SecurityException e) {
+            addInfoLog(e.getMessage() + "Exception in set file attributes");
         }
-    }
-
-    public String getDepartmentTitle(int index) {
-        if (index < departs.size()) {
-            return departs.get(index).get(1);
-        } else {
-            return null;
+        try {
+            props.load(new FileReader(file));
+        } catch (IOException e) {
+            addInfoLog(e.getMessage() + "error loading connection.properties");
         }
+        for (int i = 0; i < 16; i++) {
+            String s = "depart" + i;
+            departs.add(props.getProperty(s));
+        }
+
     }
 
     public String getDepartmentName(int otdelIndex) {
         if (otdelIndex < departs.size()) {
-            return departs.get(otdelIndex).get(0);
+            return departs.get(otdelIndex);
         } else {
             return null;
         }
     }
 
-    public List<List<String>> getDepartments() {
+    public List<String> getDepartments() {
         return departs;
     }
 }
